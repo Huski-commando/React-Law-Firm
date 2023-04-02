adf014b7be9a3a36f670faa88b5881bd8a353a20
Huski-commando <gyan.mu@gmail.com>
2023-04-03 01:49:39 +0530

    changed index.css, package.json, and tailwind.config.js file - Replaced with starter templates

--- tailwind.config.js
+++ tailwind.config.js
@@ -1,37 +1,39 @@
 module.exports = {
-  content: ["./src/**/*.{js,jsx,,ts,tsx}"],
+  content: ['./src/**/*.{js,jsx,ts,tsx}'],
   theme: {
     fontFamily: {
-      primary: "Playfair Display",
-      body: "Open Sans",
+      primary: 'Playfair Display',
+      body: 'Open Sans',
     },
     screens: {
-      sm: "640px",
-      md: "768px",
-      lg: "1024px",
-      xl: "1170px",
+      sm: '640px',
+      md: '768px',
+      lg: '1024px',
+      xl: '1170px',
     },
     extend: {
-      color: {
+      colors: {
         primary: {
-          DEFAULT: "#292A4A",
-          darker: "#242546",
-          hover: "#383956",
-        },
-        accent: {
-          DEFAULT: "#CF9455",
-          hover: "#BB864E",
-        },
-        gray: "#766F66",
-        white: "#ffffff",
-        spacing: {
-          54: "54px",
-          800: "800px",
+          DEFAULT: '#292A4A',
+          darker: '#242546',
+          hover: '#383956',
         },
 
-        content: {
-          quote: 'url("assets/img/quoteMark.png")',
+        accent: {
+          DEFAULT: '#CF9455',
+          hover: '#BB864E',
         },
+
+        gray: '#766F66',
+        white: '#ffffff',
+      },
+      spacing: {
+        54: '54px',
+        800: '800px',
+      },
+
+      content: {
+        quote: 'url("assets/img/quoteMark.png")',
       },
     },
   },

7293b888896466bbd679f127f0dac7462801309c
Huski-commando <gyan.mu@gmail.com>
2023-04-03 14:26:02 +0530

    Created index.js for assets folder and created static data in data.js

---
+++ src/data.js
@@ -0,0 +1,150 @@
+// import profile images
+import { ProfilePhoto1, ProfilePhoto2, ProfilePhoto3 } from "../src/assets/img";
+
+// import about icons
+import {
+  Skill1,
+  Skill2,
+  Skill3,
+  Skill4,
+  Skill5,
+  Skill6,
+} from "../src/assets/img";
+
+// import team images
+import { TeamMember1, TeamMember2, TeamMember3 } from "../src/assets/img";
+
+// import social icons
+import {
+  Facebook,
+  Twitter,
+  Instagram,
+  Youtube,
+  Pinterest,
+} from "../src/assets/img";
+
+// Navigation Data
+export const navigation = [
+  {
+    name: "Home",
+    href: "home",
+  },
+  {
+    name: "What we do",
+    href: "skills",
+  },
+  {
+    name: "Our team",
+    href: "team",
+  },
+  {
+    name: "Contact us",
+    href: "contact",
+  },
+];
+
+// Testimonials data
+export const testimonials = [
+  {
+    text: "Lorem ipsum dolor sit, amet consectetur adipisicing elit. Dolorem rem ad consequuntur similique sequi illum dolorum omnis inventore labore odio!",
+    name: "Jane Cooper",
+    image: ProfilePhoto1,
+    position: "Web Developer",
+  },
+  {
+    text: "Lorem ipsum dolor sit, amet consectetur adipisicing elit. Dolorem rem ad consequuntur similique sequi illum dolorum omnis inventore labore odio!",
+    name: "Theresa Webb",
+    image: ProfilePhoto2,
+    position: "Web Designer",
+  },
+  {
+    text: "Lorem ipsum dolor sit, amet consectetur adipisicing elit. Dolorem rem ad consequuntur similique sequi illum dolorum omnis inventore labore odio!",
+    name: "Kathryn Murphy",
+    image: ProfilePhoto3,
+    position: "UI/UX Designer",
+  },
+];
+
+//Skills Data
+export const skills = [
+  {
+    icons: Skill1,
+    title: "Corruption",
+    description:
+      "Lorem ipsum dolor sit amet, consectetur adipiscing elit. Sed erat nibh tristique ipsum.",
+  },
+  {
+    icons: Skill2,
+    title: "Employment",
+    description:
+      "Lorem ipsum dolor sit amet, consectetur adipiscing elit. Sed erat nibh tristique ipsum.",
+  },
+  {
+    icons: Skill3,
+    title: "Real Estate",
+    description:
+      "Lorem ipsum dolor sit amet, consectetur adipiscing elit. Sed erat nibh tristique ipsum.",
+  },
+  {
+    icons: Skill4,
+    title: "Taxes",
+    description:
+      "Lorem ipsum dolor sit amet, consectetur adipiscing elit. Sed erat nibh tristique ipsum.",
+  },
+  {
+    icons: Skill5,
+    title: "Corporate",
+    description:
+      "Lorem ipsum dolor sit amet, consectetur adipiscing elit. Sed erat nibh tristique ipsum.",
+  },
+  {
+    icons: Skill6,
+    title: "Banking",
+    description:
+      "Lorem ipsum dolor sit amet, consectetur adipiscing elit. Sed erat nibh tristique ipsum.",
+  },
+];
+
+// Team data
+export const team = [
+  {
+    image: TeamMember1,
+    name: "Jane William",
+    position: "Band & Financial Lawyer",
+    description:
+      "Lorem ipsum dolor sit amet, consectetur adipiscing elit. Sed erat nibh tristique ipsum.",
+  },
+  {
+    image: TeamMember2,
+    name: "Andrea Collins",
+    position: "Family Lawyer",
+    description:
+      "Lorem ipsum dolor sit amet, consectetur adipiscing elit. Sed erat nibh tristique ipsum.",
+  },
+  {
+    image: TeamMember3,
+    name: "John Smith",
+    position: "Bussiness Lawyer",
+    description:
+      "Lorem ipsum dolor sit amet, consectetur adipiscing elit. Sed erat nibh tristique ipsum.",
+  },
+];
+
+// social data
+export const data = [
+  {
+    icon: Facebook,
+  },
+  {
+    icon: Twitter,
+  },
+  {
+    icon: Instagram,
+  },
+  {
+    icon: Youtube,
+  },
+  {
+    icon: Pinterest,
+  },
+];
